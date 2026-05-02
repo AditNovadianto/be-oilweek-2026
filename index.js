498ed9764756324bcaca662492e0c97a8f049e81
AditNovadianto <aditya.novadianto@gmail.com>
2026-05-02 23:58:46 +0700

    initialization project, install all requirement package, and setup for index.js and db.js

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { db } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+// Test database SQL connection
+async function testDBConnection() {
+  try {
+    await db.query("SELECT 1");
+    console.log("✅ Database SQL connected successfully!");
+    return true;
+  } catch (error) {
+    console.error("❌ Failed to connect to the database:", error.message);
+    return false;
+  }
+}
+
+testDBConnection();
+//
+
+// Test database MongoDB connection
+// connectMongoDB();
+//
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the OilWeek 2026 API");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
